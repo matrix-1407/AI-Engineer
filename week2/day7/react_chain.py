b05e407cc90a76a67486846f49fb1105262ec812
matrix-1407 <mrudulbokade1407@gmail.com>
2026-08-21 13:48:36 +0530

    day 7: Reasoning+Action

--- week2/day7/react_chain.py
+++ week2/day7/react_chain.py
@@ -9,7 +9,7 @@ load_dotenv()
 my_api_key=os.getenv("GROQ_API_KEY")
 
 if not my_api_key:
-    raise ValueError("API key kaha hai bhai")
+    raise ValueError("API key not found.")
 
 client=Groq(api_key=my_api_key)
 model="llama-3.3-70b-versatile"
